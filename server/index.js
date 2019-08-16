fd537ae00016a31a24ae7af743ed61cd10df7b36
Raven-Lee Mills <rayvenmills.1998@gmail.com>
2019-08-16 07:07:30 -0300

    Commit-02 - Added static 'public' folder to express server with updated config for prod - Added bare bones of vue webpage, code for webpage still needs to be updated

--- server/index.js
+++ server/index.js
@@ -10,5 +10,10 @@ app.use(cors());
 const students = require('./routes/API/students');
 app.use('/api/students', students);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+    app.get(/.*/, (request, response) => response.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
